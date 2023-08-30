78058d0bc99363911392698916ecc4594f7bdfca
TheGerrex <gerrex95@gmail.com>
2023-08-29 18:41:37 -0600

    agrege botones para quitar ambas columnas a voluntad

--- src/app/chat/pages/chat-page/chat-page.component.ts
+++ src/app/chat/pages/chat-page/chat-page.component.ts
@@ -8,17 +8,40 @@ import { PromptLibraryComponent } from '../../components/prompt-library/prompt-l
 })
 export class ChatPageComponent implements OnInit {
   @ViewChild(PromptLibraryComponent) modal!: PromptLibraryComponent;
-  sidenavOpen = false;
-
-  toggleSidenav() {
-    this.sidenavOpen = !this.sidenavOpen;
+  columnChatsOpen = true;
+  columnPluginsOpen = true;
+  columnPluginsOpenTablet = false;
+  
+  toggleChats() {
+    this.columnChatsOpen = !this.columnChatsOpen;
+    if (  window.innerWidth >= 768 && window.innerWidth <= 992) {
+      this.columnPluginsOpenTablet = false;
+      // this.columnChatsOpen = !this.columnChatsOpen;
+    }
+    console.log("toggleChats")
+    console.log("Column chats", this.columnChatsOpen)
+    console.log("Column plugin", this.columnPluginsOpen)
+    console.log("Column plugin tablet", this.columnPluginsOpenTablet)
+  }
+  togglePlugins() {
+    this.columnPluginsOpen = !this.columnPluginsOpen;
+    
+    if (  window.innerWidth >= 768 && window.innerWidth <= 992) {
+      this.columnChatsOpen = false;
+      this.columnPluginsOpenTablet = !this.columnPluginsOpenTablet;
+    }
+    console.log("togglePlugins")
+    console.log("Column chats", this.columnChatsOpen)
+    console.log("Column plugin", this.columnPluginsOpen)
+    console.log("Column plugin tablet", this.columnPluginsOpenTablet)
   }
 
   ngOnInit() {
     // Close the sidenav on initialization for smaller screens
-    if (window.innerWidth <= 768) {
-      this.sidenavOpen = false;
-    }
+    // if (  window.innerWidth <= 768 && window.innerWidth >= 992) {
+    //   this.columnChatsOpen = false;
+    // }
+    
   }
 
   openModal() {
