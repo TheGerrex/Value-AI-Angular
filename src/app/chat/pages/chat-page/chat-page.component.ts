84707aa8e3d3a137a33fbd84578115dd106308d6
TheGerrex <gerrex95@gmail.com>
2023-08-29 18:47:51 -0600

    pequeño cambio

--- src/app/chat/pages/chat-page/chat-page.component.ts
+++ src/app/chat/pages/chat-page/chat-page.component.ts
@@ -16,12 +16,8 @@ export class ChatPageComponent implements OnInit {
     this.columnChatsOpen = !this.columnChatsOpen;
     if (  window.innerWidth >= 768 && window.innerWidth <= 992) {
       this.columnPluginsOpenTablet = false;
-      // this.columnChatsOpen = !this.columnChatsOpen;
     }
-    console.log("toggleChats")
-    console.log("Column chats", this.columnChatsOpen)
-    console.log("Column plugin", this.columnPluginsOpen)
-    console.log("Column plugin tablet", this.columnPluginsOpenTablet)
+    
   }
   togglePlugins() {
     this.columnPluginsOpen = !this.columnPluginsOpen;
@@ -30,17 +26,9 @@ export class ChatPageComponent implements OnInit {
       this.columnChatsOpen = false;
       this.columnPluginsOpenTablet = !this.columnPluginsOpenTablet;
     }
-    console.log("togglePlugins")
-    console.log("Column chats", this.columnChatsOpen)
-    console.log("Column plugin", this.columnPluginsOpen)
-    console.log("Column plugin tablet", this.columnPluginsOpenTablet)
   }
 
   ngOnInit() {
-    // Close the sidenav on initialization for smaller screens
-    // if (  window.innerWidth <= 768 && window.innerWidth >= 992) {
-    //   this.columnChatsOpen = false;
-    // }
     
   }
 
